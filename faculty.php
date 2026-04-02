08867f3c193d8be71bffcbd83403dcaed73751c4
1jackk <144811952+1jackk@users.noreply.github.com>
2026-04-02 17:45:12 -0400

    Refactor faculty management into Faculty class

--- faculty.php
+++ faculty.php
@@ -1,59 +1,30 @@
 <?php
-require 'db.php';
-
-if (isset($_GET['delete'])) {
-    $stmt = $pdo->prepare("DELETE FROM faculty WHERE faculty_id = ?");
-    $stmt->execute([$_GET['delete']]);
+class Faculty {
+ 
+    public static function getAll($pdo) {
+        $stmt = $pdo->query("SELECT * FROM faculty");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+ 
+    public static function insert($pdo, $name, $email) {
+        $stmt = $pdo->prepare("INSERT INTO faculty (name, email) VALUES (:name, :email)");
+        $stmt->bindValue(':name', $name);
+        $stmt->bindValue(':email', $email);
+        $stmt->execute();
+    }
+ 
+    public static function update($pdo, $id, $name, $email){
+        $stmt = $pdo->prepare("UPDATE faculty SET name = :name, email = :email WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->bindValue(':name', $name);
+        $stmt->bindValue(':email', $email);
+        $stmt->execute();
+    }
+ 
+    public static function delete($pdo, $id){
+        $stmt = $pdo->prepare("DELETE FROM faculty WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
 }
-
-if (isset($_POST['add'])) {
-    $stmt = $pdo->prepare("INSERT INTO faculty (first_name, last_name, department) VALUES (?, ?, ?)");
-    $stmt->execute([$_POST['first_name'], $_POST['last_name'], $_POST['department']]);
-}
-
-if (isset($_POST['update'])) {
-    $stmt = $pdo->prepare("UPDATE faculty SET first_name = ?, last_name = ?, department = ? WHERE faculty_id = ?");
-    $stmt->execute([$_POST['first_name'], $_POST['last_name'], $_POST['department'], $_POST['faculty_id']]);
-}
-
-$faculty = $pdo->query("SELECT * FROM faculty")->fetchAll();
-?>
-<!DOCTYPE html>
-<html>
-<head><title>Faculty</title></head>
-<body>
-<h1>Faculty</h1>
-<a href="index.php">Home</a>
-
-<h3>Add Faculty</h3>
-<form method="POST">
-First Name: <input type="text" name="first_name">
-Last Name: <input type="text" name="last_name">
-Department: <input type="text" name="department">
-<button name="add">Add</button>
-</form>
-
-<h3>Update Faculty</h3>
-<form method="POST">
-Faculty ID: <input type="number" name="faculty_id">
-First Name: <input type="text" name="first_name">
-Last Name: <input type="text" name="last_name">
-Department: <input type="text" name="department">
-<button name="update">Update</button>
-</form>
-
-<h3>Faculty List</h3>
-<table border="1">
-<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Department</th><th>Actions</th></tr>
-<?php foreach($faculty as $f): ?>
-<tr>
-    <td><?php echo $f['faculty_id']; ?></td>
-    <td><?php echo $f['first_name']; ?></td>
-    <td><?php echo $f['last_name']; ?></td>
-    <td><?php echo $f['department']; ?></td>
-    <td><a href="faculty.php?delete=<?php echo $f['faculty_id']; ?>">Delete</a></td>
-</tr>
-<?php endforeach; ?>
-</table>
-</body>
-</html>
+ 
