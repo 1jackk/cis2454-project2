884c0957304dccc7fb270ce9f5d8abeb6296974b
1jackk <144811952+1jackk@users.noreply.github.com>
2026-04-02 17:45:38 -0400

    Refactor section management into Section class

--- section.php
+++ section.php
@@ -1,59 +1,32 @@
 <?php
-require 'db.php';
-
-if(isset($_POST['add'])){
-    $stmt = $pdo->prepare("INSERT INTO section (course_id, faculty_id, semester) VALUES (?, ?, ?)");
-    $stmt->execute([$_POST['course_id'], $_POST['faculty_id'], $_POST['semester']]);
+class Section {
+ 
+    public static function getAll($pdo){
+        $stmt = $pdo->query("SELECT * FROM section");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+ 
+    public static function insert($pdo, $course_code, $faculty_id, $semester) {
+        $stmt = $pdo->prepare("INSERT INTO section (course_code, faculty_id, semester) VALUES (:course_code, :faculty_id, :semester)");
+        $stmt->bindValue(':course_code', $course_code);
+        $stmt->bindValue(':faculty_id', $faculty_id);
+        $stmt->bindValue(':semester', $semester);
+        $stmt->execute();
+    }
+ 
+    public static function update($pdo, $id, $course_code, $faculty_id, $semester){
+        $stmt = $pdo->prepare("UPDATE section SET course_code = :course_code, faculty_id = :faculty_id, semester = :semester WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->bindValue(':course_code', $course_code);
+        $stmt->bindValue(':faculty_id', $faculty_id);
+        $stmt->bindValue(':semester', $semester);
+        $stmt->execute();
+    }
+ 
+    public static function delete($pdo, $id) {
+        $stmt = $pdo->prepare("DELETE FROM section WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
 }
-
-if(isset($_POST['update'])){
-    $stmt = $pdo->prepare("UPDATE section SET course_id = ?, faculty_id = ?, semester = ? WHERE section_id = ?");
-    $stmt->execute([$_POST['course_id'], $_POST['faculty_id'], $_POST['semester'], $_POST['section_id']]);
-}
-
-if(isset($_GET['delete'])){
-    $stmt = $pdo->prepare("DELETE FROM section WHERE section_id = ?");
-    $stmt->execute([$_GET['delete']]);
-}
-
-$sections = $pdo->query("SELECT * FROM section")->fetchAll();
-?>
-<!DOCTYPE html>
-<html>
-<head><title>Sections</title></head>
-<body>
-<h1>Sections</h1>
-<a href="index.php">Home</a>
-
-<h2>Add Section</h2>
-<form method="POST">
-Course ID: <input type="number" name="course_id">
-Faculty ID: <input type="number" name="faculty_id">
-Semester: <input type="text" name="semester">
-<button name="add">Add</button>
-</form>
-
-<h2>Update Section</h2>
-<form method="POST">
-Section ID: <input type="number" name="section_id">
-Course ID: <input type="number" name="course_id">
-Faculty ID: <input type="number" name="faculty_id">
-Semester: <input type="text" name="semester">
-<button name="update">Update</button>
-</form>
-
-<h2>All Sections</h2>
-<table border="1">
-<tr><th>Section ID</th><th>Course ID</th><th>Faculty ID</th><th>Semester</th><th></th></tr>
-<?php foreach($sections as $s){ ?>
-<tr>
-    <td><?php echo $s['section_id']; ?></td>
-    <td><?php echo $s['course_id']; ?></td>
-    <td><?php echo $s['faculty_id']; ?></td>
-    <td><?php echo $s['semester']; ?></td>
-    <td><a href="section.php?delete=<?php echo $s['section_id']; ?>">Delete</a></td>
-</tr>
-<?php } ?>
-</table>
-</body>
-</html>
+ 
